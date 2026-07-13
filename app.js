d0c338a13f7a23d7f2c612b48cbfbec274586e4b
amurvvs-max <amurvvs@gmail.com>
2026-07-13 20:00:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM загружен');
+  
   auth.onAuthStateChanged(user => {
     if (user) {
+      console.log('Пользователь авторизован:', user.uid);
+      
+      console.log('Запуск навигации...');
       initNavigation();
+      
+      console.log('Запуск копилки...');
       initSavings();
+      
+      console.log('Запуск авто...');
       initCar();
+      
+      console.log('Запуск заметок...');
       initNotes();
+      
+      console.log('Всё инициализировано');
+    } else {
+      console.log('Пользователь НЕ авторизован');
     }
   });
 });
