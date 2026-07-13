83ebdb18c56d6ef0633ac37ba1c7af756280b1d4
amurvvs-max <amurvvs@gmail.com>
2026-07-13 19:53:00 +0300

    Add files via upload

---
+++ car.js
@@ -0,0 +1,7 @@
+function initCar() {
+  document.getElementById('car-page').innerHTML = `
+    <div class="empty-state">
+      <div class="empty-icon">🚗</div>
+      <p>Раздел в разработке</p>
+    </div>`;
+}
---
+++ notes.js
@@ -0,0 +1,7 @@
+function initNotes() {
+  document.getElementById('notes-page').innerHTML = `
+    <div class="empty-state">
+      <div class="empty-icon">📝</div>
+      <p>Раздел в разработке</p>
+    </div>`;
+}
